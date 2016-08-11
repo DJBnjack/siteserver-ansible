52776bae4c73fb885eea06dfdc7edfc09ac05379
Ubuntu <johannes@AnsibleHost.y44xjdgihmzejezbxjbi1v0shd.ax.internal.cloudapp.net>
2016-08-11 09:15:15 +0000

    Added ansible and ansible-docker install

--- install-reqs.sh
+++ install-reqs.sh
@@ -1,2 +1,7 @@
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install ansible -y
+ansible-galaxy install angstwad.docker_ubuntu
 ansible-galaxy install evrardjp.haproxy
 
