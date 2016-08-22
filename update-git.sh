5b1c0e4a78cc03479f535821c9b5c41b555ce41b
Johannes <johannes.bertens@gmail.com>
2016-08-22 19:31:41 +0000

    seems to work

--- update-git.sh
+++ update-git.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo '[user]' >> /home/johannes/.gitconfig
 echo '        name = Johannes' >> /home/johannes/.gitconfig
 echo '        email = johannes.bertens@gmail.com' >> /home/johannes/.gitconfig
 
